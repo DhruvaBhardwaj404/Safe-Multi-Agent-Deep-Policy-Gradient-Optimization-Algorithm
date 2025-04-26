2f14fd8269e30072e0e7c363fbd7e754885f9e8b
DhruvaBhardwaj404 <ghost404@Daarshanik>
2025-04-26 23:23:39 +0530

    changed policy network and q networks

--- src/DQN.py
+++ src/DQN.py
@@ -6,10 +6,13 @@ class DQN(nn.Module):
 
     def __init__(self, input_dim:int,device):
         super().__init__()
-        h1  = int(input_dim/3)
+        h1 = int(input_dim * 2)
+        h2 = int(input_dim)
         self.model = nn.Sequential( nn.Linear(input_dim,h1),
                                     nn.ReLU(),
-                                    nn.Linear(h1,1)).to(device)
+                                    nn.Linear(h1, h2),
+                                    nn.ReLU(),
+                                    nn.Linear(h2,1)).to(device)
 
 
     def forward(self, x):
